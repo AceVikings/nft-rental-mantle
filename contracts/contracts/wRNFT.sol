a5e548b1146029d75c0fdadbd2bfd66ab02a511a
Ace <51465755+AceVikings@users.noreply.github.com>
2023-02-22 18:23:25 +0530

    wrapper contract added

--- contracts/contracts/wRNFT.sol
+++ contracts/contracts/wRNFT.sol
@@ -7,7 +7,30 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721Enumer
 contract wRNFT is ERC721EnumerableUpgradeable {
     IFullERC721 rootNFT;
 
+    struct rentInfo {
+        address renter;
+        uint rentStartTime;
+        uint duration;
+    }
+
+    struct approveInfo {
+        address renter;
+        uint duration;
+    }
+
     mapping(uint => address) public tokenOwner;
+    mapping(uint => approveInfo) public approvedRenter;
+    mapping(uint => rentInfo) public rentalInfo;
+
+    modifier notRented(uint tokenId) {
+        require(
+            block.timestamp >
+                rentalInfo[tokenId].rentStartTime +
+                    rentalInfo[tokenId].duration,
+            "Currently rented"
+        );
+        _;
+    }
 
     function init(address _nft) external initializer {
         rootNFT = IFullERC721(_nft);
@@ -24,12 +47,48 @@ contract wRNFT is ERC721EnumerableUpgradeable {
         _mint(msg.sender, tokenId);
     }
 
-    // function rent()
+    function approveRental(
+        uint tokenId,
+        address to,
+        uint duration
+    ) external notRented(tokenId) {
+        require(tokenOwner[tokenId] == msg.sender, "Not owner");
+        require(to != address(0), "Can't approve address 0");
+        approvedRenter[tokenId] = approveInfo(to, duration);
+    }
 
-    function unWrap(uint tokenId) external {
+    function rentToken(uint tokenId) external notRented(tokenId) {
+        require(_exists(tokenId), "Token doesn't exist");
+        require(approvedRenter[tokenId].renter == msg.sender, "Not approved");
+        rentalInfo[tokenId] = rentInfo(
+            msg.sender,
+            block.timestamp,
+            approvedRenter[tokenId].duration
+        );
+    }
+
+    function unWrap(uint tokenId) external notRented(tokenId) {
         require(tokenOwner[tokenId] == msg.sender, "Not owner");
         rootNFT.transferFrom(address(this), msg.sender, tokenId);
         delete tokenOwner[tokenId];
         _burn(tokenId);
+    }
+
+    function ownerOf(
+        uint256 tokenId
+    )
+        public
+        view
+        override(IERC721Upgradeable, ERC721Upgradeable)
+        returns (address)
+    {
+        if (
+            block.timestamp <=
+            rentalInfo[tokenId].rentStartTime + rentalInfo[tokenId].duration
+        ) {
+            return rentalInfo[tokenId].renter;
+        } else {
+            super.ownerOf(tokenId);
+        }
     }
 }
